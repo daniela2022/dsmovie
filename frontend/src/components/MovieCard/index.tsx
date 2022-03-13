0a6be063cf9909f9ceba96c3ea8ff0d21942a982
Daniela Pedon <danielapedon3@gmail.com>
2022-03-12 21:46:19 -0300

    Security config

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -5,8 +5,8 @@ function MovieCard() {
 
     const movie = {
         id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
+        image: "https://www.themoviedb.org/t/p/original/i0uajcHH9yogXMfDHpOXexIukG9.jpg",
+        title: "PEAKY BLINDERS",
         count: 2,
         score: 4.5
     };
